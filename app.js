9efb1e77af364f586c5da89ebcf10c61fa3369cd
mmb1214 <78535223+mmb1214@users.noreply.github.com>
2022-09-19 23:44:42 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,45 @@
+var http = require('http');
+var fs = require('fs');
+var index = fs.readFileSync( 'index.html');
+
+var SerialPort = require('serialport');
+const parsers = SerialPort.parsers;
+const parser = new parsers.Readline({
+  delimiter: '\r\n'
+});
+
+var port = new SerialPort('/dev/tty.usbmodem14101',{ 
+  baudRate: 9600,
+  dataBits: 8,
+  parity: 'none',
+  stopBits: 1,
+  flowControl: false
+});
+
+port.pipe(parser);
+
+
+
+var app = http.createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(index);
+});
+
+var io = require('socket.io').listen(app);
+
+io.on('connection', function(data){
+    console.log('Node.js is listening');
+});
+
+
+parser.on('data', function(data) {
+    
+    console.log('Received data from port: ' + data);
+    io.emit('data', data);
+    
+      
+});
+
+
+app.listen(3000);
+
